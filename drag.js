ed5e3f804d6bb96631d3172860bff99e78e48df0
tuhoufu <670962060@qq.com>
2017-10-08 19:45:05 +0800

    success drag

--- drag.js
+++ drag.js
@@ -5,25 +5,20 @@ function drag(id){
 	var disX=0;
 	var disY=0;
 	obj.onmousedown=function(ev){
-<<<<<<< HEAD
 		var ev=ev||even;	
 		disX=ev.pageX-obj.offsetLeft;
-		disY=ev.page.Y-obj.offsetTop;
+		disY=ev.pageY-obj.offsetTop;
 		document.onmousemove=function(ev){
 			var ev=ev||even;
 		  obj.style.left=ev.pageX-disX+'px';
 		  obj.style.top=ev.pageY-disY+'px';	
 		 	
 		};
-=======
-		var ev=ev||even;
-		 disX=ev.pageX-obj.offsetLeft;
-		 disY=ev.pageY-obj.offsetTop;
-		document.onmousemove=function(ev){
-			var ev=ev||even;
-			obj.style.left=ev.clientX-disX+'px';
-			obj.style.top=ev.clientY-disY+'px';
-		}
->>>>>>> new1
+		document.onmouseup=function(){
+			document.onmousemove=null;
+			document.onmouseup=null;
+		};
+
 	}
+	return false;
 }
