192d59a5a84a13a3e8cd05aa27f0067cd9d8b291
tuhoufu <670962060@qq.com>
2017-10-08 15:51:24 +0800

    change drag.js form new1

--- drag.js
+++ drag.js
@@ -5,6 +5,13 @@ function drag(id){
 	var disX=0;
 	var disY=0;
 	obj.onmousedown=function(ev){
-		var ev=ev||even;	
+		var ev=ev||even;
+		 disX=ev.pageX-obj.offsetLeft;
+		 disY=ev.pageY-obj.offsetTop;
+		document.onmousemove=function(ev){
+			var ev=ev||even;
+			obj.style.left=ev.clientX-disX+'px';
+			obj.style.top=ev.clientY-disY+'px';
+		}
 	}
 }
