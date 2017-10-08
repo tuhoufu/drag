94e3100edebb74fd0df2f878317c938c23dd90ee
tuhoufu <670962060@qq.com>
2017-10-08 15:47:52 +0800

    change drag,js form master

--- drag.js
+++ drag.js
@@ -6,5 +6,13 @@ function drag(id){
 	var disY=0;
 	obj.onmousedown=function(ev){
 		var ev=ev||even;	
+		disX=ev.pageX-obj.offsetLeft;
+		disY=ev.page.Y-obj.offsetTop;
+		document.onmousemove=function(ev){
+			var ev=ev||even;
+		  obj.style.left=ev.pageX-disX+'px';
+		  obj.style.top=ev.pageY-disY+'px';	
+		 	
+		};
 	}
 }
